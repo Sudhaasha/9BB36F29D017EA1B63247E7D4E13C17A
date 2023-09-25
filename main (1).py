ede364956386300e4b80800db08fe92467649d5e
Sudhaasha <144528130+Sudhaasha@users.noreply.github.com>
2023-09-25 21:43:10 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,20 +1,25 @@
-# leap year
-"""
-year / 4 == 0 &
-year / 100 != 0|
-year / 400 == 0
-"""
+class student:
+  
+  def __init__(self,name,roll_number,cgpa):
+    self.name=name
+    self.roll_number=roll_number
+    self.cgpa=cgpa
 
+def sort_students (student_list):
+# sort the list of students in descending order of the cgpa     
+    
+    sorted_students=sorted(student_list, key=lambda student:  student.cgpa ,reverse=True)
 
-def isleapyear(year):
-  if (year / 4 == 0 and year / 100 != 0) or year / 400 == 0:
-    return True
-  else:
-    return False
+    return sorted_students
 
-
-year = 2020
-if isleapyear(year):
-  print("2020 is a leapyear")
-else:
-  print("2020 is not a leapyear")
+# example usage
+students=[ 
+  student ("karishma","3456",8.7),
+  student("veni","3457",8.6),
+  student("selvakumar","3458",8.9), 
+  student("kishore","3459",8.5),
+  student ("krishnai","3460",8.8),]
+sorted_students=sort_students(students)
+#print the sorted list of students
+for student in sorted_students:
+  print("Name:{},Roll number:{},CGPA:{}".format(student.name,student.roll_number,student.cgpa))
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# 1.1 Implemens a function to calculate a factorial.
-def fact_rec(n):
-  if n==0 or n==1:
-     return 1
-  else:
-    return n*fact_rec(n-1)
-number=4
-result=fact_rec(number)
-print("The factorial of 4 is",result)
+def linearSearchProduct(productList,targetProduct):
+  indices=[]
+  for index,product in enumerate(productList):
+    if product==targetProduct:
+      indices.append(index)
+  return indices
+#example usage
+products=["shoes","boots","loafer","shoes","sandals","shoes"]
+target1="shoes"
+target2="apples"
+result=linearSearchProduct(products,target1)
+print(result)
